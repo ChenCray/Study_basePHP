4eb0c411e690cd339c24cef62a584f0617cc6273
ChenCray <Cray17292@gmail.com>
2018-05-24 14:39:49 +0800

    Update Study if,for,foreach,switch,while

--- Study.php
+++ Study.php
@@ -1,4 +1,6 @@
 <?php
+/* 變數、常數、函式
+-----------------*/
   $test = 123;
   define("PI", 3.1415926);
 
@@ -18,4 +20,62 @@
 
   rt_HelloWorld();
   rt_HelloWorld();
+
+// 判斷式
+  function check_isPI($input){
+    if ($input == PI){
+      echo "Is PI<br>";
+    }
+    else {
+      echo "Is not PI<br>";
+    }
+  }
+
+  check_isPI(3.1415926);
+  check_isPI("abc");
+
+  $tmp_array = ["abc", "123", 456, PI];
+
+  for($count=0; $count<3; ++$count){
+    if ($count == 2){
+      echo $tmp_array[$count] . "<br>";
+    }
+    else {
+      echo $tmp_array[$count] . "-";
+    }
+  }
+
+  foreach ($tmp_array as $key => $value) {
+    echo "key=$key.    value=$value<br>";
+  }
+
+  function switch_test($input){
+    switch ($input) {
+      case '1':
+        echo "Is 1<br>";
+        break;
+      case '2':
+        echo "Is 2<br>";
+        break;
+      default:
+        echo "Is other<br>";
+        break;
+    }
+  }
+
+  switch_test(1);
+  switch_test("2");
+  switch_test("abc");
+
+  function add_toInput($input){
+    $start = 0;
+    $rt = 0;
+    while($start <= $input){
+      $rt += $start;
+      ++$start;
+    }
+    return $rt;
+  }
+
+  echo add_toInput(10);
  ?>
